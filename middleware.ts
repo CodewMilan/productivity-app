d58271bae60cab2a9ef265fd87cd21c0c099b8ea
milan <codewmilan@gmail.com>
2025-09-02 19:59:24 +0530

    Revert "deployment fix 3 :("
    This reverts commit 2f9cee38eb698f0f95695c63c976660ddf4bcb26.

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,8 @@ import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs"
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
-  const supabase = createMiddlewareClient({ req, res })
 
+  const supabase = createMiddlewareClient({ req, res })
   const {
     data: { session },
   } = await supabase.auth.getSession()
